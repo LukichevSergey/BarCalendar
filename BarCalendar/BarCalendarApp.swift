36af7bf53ee8ec3d6dc6571bee4faf04c8aa40be
Sergey Lukichev <Lukichev.sk@dns-shop.ru>
2026-06-25 10:59:35 +0700

    Initial Commit

---
+++ BarCalendar/BarCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCalendarApp.swift
+//  BarCalendar
+//
+//  Created by LukichevSergey on 25.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct BarCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
